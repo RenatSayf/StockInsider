a99d91e2db6962a2d5e54f73fa93a777d79ad827
renatsayf <renatsayf@gmail.com>
2020-01-16 19:44:05 +0500

    SearchRequest.kt refatoring

--- app/src/main/java/com/renatsayf/stockinsider/models/SearchRequest.kt
+++ app/src/main/java/com/renatsayf/stockinsider/models/SearchRequest.kt
@@ -2,6 +2,7 @@ package com.renatsayf.stockinsider.models
 
 import com.renatsayf.stockinsider.network.OpenInsiderService
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -9,7 +10,7 @@ import javax.inject.Inject
 
 class SearchRequest @Inject constructor()
 {
-    var openInsiderService: OpenInsiderService = OpenInsiderService.create()
+    private var openInsiderService: OpenInsiderService = OpenInsiderService.create()
 
     var tickers: String = ""
     var filingDate: String = "0"
@@ -24,25 +25,26 @@ class SearchRequest @Inject constructor()
     var groupBy: String = ""
     var sortBy: String = ""
 
-    private var key_tickers = "s="
-    private var key_filing_date = "fd="
-    private var key_trade_date = "td="
-    private var key_is_purchase = "xp="
-    private var key_is_sale = "xs="
-    private var key_trade_min = "vl="
-    private var key_trade_max = "vh="
-    private var key_is_officer = "isofficer="
-    private var key_is_director = "isdirector="
-    private var key_is_tenpercent = "istenpercent="
-    private var key_group_by = "grp="
-    private var key_sort_by = "sortcol="
+    private var keyTickers = "s="
+    private var keyFilingDate = "fd="
+    private var keyTradeDate = "td="
+    private var keyIsPurchase = "xp="
+    private var keyIsSale = "xs="
+    private var keyTradeMin = "vl="
+    private var keyTradeMax = "vh="
+    private var keyIsOfficer = "isofficer="
+    private var keyIsDirector = "isdirector="
+    private var keyIsTenPercent = "istenpercent="
+    private var keyGroupBy = "grp="
+    private var keySortBy = "sortcol="
     private var separator = "&"
 
     private lateinit var paramsSet: String
+    private var disposable : Disposable? = null
 
     fun fetchTradingScreen()
     {
-        val res = openInsiderService.getInsiderTrading(
+        disposable = openInsiderService.getInsiderTrading(
                 tickers,
                 filingDate,
                 tradeDate,
@@ -62,11 +64,11 @@ class SearchRequest @Inject constructor()
                 isTenPercent,
                 groupBy,
                 sortBy
-                                            )
+                                                         )
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.newThread())
             .subscribe({ result ->
-                            val document = result
+                           val document = result
                            val body = document.body()
                            val tbody = body.select("#tablewrapper > table > tbody")
                            return@subscribe
@@ -77,31 +79,31 @@ class SearchRequest @Inject constructor()
 
     private fun buildRequest() : String
     {
-        key_tickers = "s=$tickers"
-        key_filing_date = "fd=$filingDate"
-        key_trade_date = "td=".plus(tradeDate)
-        key_is_purchase = "xp=".plus(isPurchase)
-        key_is_sale = "xs=".plus(isSale)
-        key_trade_min = "vl=".plus(tradedMin)
-        key_trade_max = "vh=".plus(tradedMax)
-        key_is_officer = "isofficer=".plus(isOfficer)
-        key_is_director = "isdirector=".plus(isDirector)
-        key_is_tenpercent = "istenpercent=".plus(isTenPercent)
-        key_group_by = "grp=".plus(groupBy)
-        key_sort_by = "sortcol=".plus(sortBy)
+        keyTickers = "s=$tickers"
+        keyFilingDate = "fd=$filingDate"
+        keyTradeDate = "td=".plus(tradeDate)
+        keyIsPurchase = "xp=".plus(isPurchase)
+        keyIsSale = "xs=".plus(isSale)
+        keyTradeMin = "vl=".plus(tradedMin)
+        keyTradeMax = "vh=".plus(tradedMax)
+        keyIsOfficer = "isofficer=".plus(isOfficer)
+        keyIsDirector = "isdirector=".plus(isDirector)
+        keyIsTenPercent = "istenpercent=".plus(isTenPercent)
+        keyGroupBy = "grp=".plus(groupBy)
+        keySortBy = "sortcol=".plus(sortBy)
 
-        paramsSet = "screener?$key_tickers$separator" +
-                "$key_filing_date$separator" +
-                "$key_trade_date$separator" +
-                "$key_is_purchase$separator" +
-                "$key_is_sale$separator" +
-                "$key_trade_min$separator" +
-                "$key_trade_max$separator" +
-                "$key_is_officer$separator" +
-                "$key_is_director$separator" +
-                "$key_is_tenpercent$separator" +
-                "$key_group_by$separator" +
-                "$key_sort_by&cnt=100&page=1"
+        paramsSet = "screener?$keyTickers$separator" +
+                "$keyFilingDate$separator" +
+                "$keyTradeDate$separator" +
+                "$keyIsPurchase$separator" +
+                "$keyIsSale$separator" +
+                "$keyTradeMin$separator" +
+                "$keyTradeMax$separator" +
+                "$keyIsOfficer$separator" +
+                "$keyIsDirector$separator" +
+                "$keyIsTenPercent$separator" +
+                "$keyGroupBy$separator" +
+                "$keySortBy&cnt=100&page=1"
 
         return "http://openinsider.com/".plus(paramsSet)
     }
