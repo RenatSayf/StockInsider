fe19260936a8f1a771961558d103c550bd936fb4
renatsayf <renatsayf@gmail.com>
2020-02-21 12:55:52 +0500

    RoomDataBaseModule.kt

--- app/src/main/java/com/renatsayf/stockinsider/di/modules/RoomDataBaseModule.kt
+++ app/src/main/java/com/renatsayf/stockinsider/di/modules/RoomDataBaseModule.kt
@@ -1,8 +1,8 @@
 package com.renatsayf.stockinsider.di.modules
 
 import android.content.Context
-import com.renatsayf.stockinsider.db.RoomDBProvider
 import com.renatsayf.stockinsider.db.AppDao
+import com.renatsayf.stockinsider.db.RoomDBProvider
 import dagger.Module
 import dagger.Provides
 
