d85146ebcb59663722a50b23dc61e7b44a4aaead
renatsayf <renatsayf@gmail.com>
2020-09-07 22:24:11 +0500

    mobile_navigation.xml has been updated

--- app/src/main/java/com/renatsayf/stockinsider/ui/strategy/StrategyFragment.kt
+++ app/src/main/java/com/renatsayf/stockinsider/ui/strategy/StrategyFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.renatsayf.stockinsider.MainActivity
 import com.renatsayf.stockinsider.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +17,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class StrategyFragment : Fragment() {
 
     private lateinit var strategyViewModel: StrategyViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this){
+            (activity as MainActivity).navController.navigate(R.id.nav_home)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
