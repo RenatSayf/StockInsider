4d37634f4d74b57809d74189c7631dfa7161299f
renatsayf <renatsayf@gmail.com>
2020-10-02 22:23:09 +0500

    Insert_into_search_set.sql has been updated

--- app/src/main/assets/database/Insert_into_search_set.sql
+++ app/src/main/assets/database/Insert_into_search_set.sql
@@ -1,1 +1,14 @@
-INSERT INTO search_set (set_name, company_name, ticker, filing_period, trade_period, is_purchase, is_sale, trade_min, trade_max, is_officer, is_director, is_ten_percent, group_by, sort_by) VALUES ("purchases 1 for week", "", "", 7, 7, 1, 0, 999, "", 1, 1, 1, 0, 0)
+INSERT INTO search_set (set_name, 
+company_name, 
+ticker, 
+filing_period, 
+trade_period, 
+is_purchase, 
+is_sale, 
+trade_min, 
+trade_max, 
+is_officer, 
+is_director, 
+is_ten_percent, 
+group_by, 
+sort_by) VALUES ("Purchases more $5 million for week", "", "", 7, 7, 1, 0, 4999, "", 1, 1, 1, 0, 0)
