e8842783e0306638f74e486bcfb896e59622e7c9
renatsayf <renatsayf@gmail.com>
2020-10-06 13:43:24 +0500

    unused files have been removed

--- app/src/main/java/com/renatsayf/stockinsider/ui/latest/purchases25/Purchases25ViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.renatsayf.stockinsider.ui.latest.purchases25
-
-import androidx.lifecycle.ViewModel
-
-class Purchases25ViewModel : ViewModel()
-{
-    // TODO: Implement the ViewModel
-}
