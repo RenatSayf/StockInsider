f1c2af8e8c8bebe22d12732b65b4313938c14e58
renatsayf <renatsayf@gmail.com>
2020-11-05 22:20:07 +0500

    IDataRepository.kt has been updated

--- app/src/main/java/com/renatsayf/stockinsider/repository/IDataRepository.kt
+++ app/src/main/java/com/renatsayf/stockinsider/repository/IDataRepository.kt
@@ -4,11 +4,12 @@ import com.renatsayf.stockinsider.db.Companies
 import com.renatsayf.stockinsider.db.RoomSearchSet
 import com.renatsayf.stockinsider.models.Deal
 import com.renatsayf.stockinsider.models.SearchSet
+import io.reactivex.Observable
 import io.reactivex.Single
 
 interface IDataRepository
 {
-    fun getTradingScreenFromNetAsync(set: SearchSet) : io.reactivex.Observable<ArrayList<Deal>>
+    fun getTradingScreenFromNetAsync(set: SearchSet) : Observable<ArrayList<Deal>>
 
     fun getInsiderTradingFromNetAsync(insider: String) : Single<ArrayList<Deal>>
 
