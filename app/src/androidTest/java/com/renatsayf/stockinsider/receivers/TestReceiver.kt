c3d58a8b22479d11dbc7c939d49e3ab4ebb83921
Renat <renatsayf@gmail.com>
2023-01-08 13:29:47 +0500

    TestReceiver.kt - refactoring

--- app/src/androidTest/java/com/renatsayf/stockinsider/receivers/TestReceiver.kt
+++ app/src/androidTest/java/com/renatsayf/stockinsider/receivers/TestReceiver.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import com.renatsayf.stockinsider.schedule.Scheduler
 import com.renatsayf.stockinsider.service.ServiceNotification
+import org.junit.Assert
 
 class TestReceiver : AlarmReceiver() {
 
@@ -12,6 +13,7 @@ class TestReceiver : AlarmReceiver() {
 
             if (intent.action == Scheduler.ONE_SHOOT_ACTION || intent.action == Scheduler.REPEAT_SHOOT_ACTION) {
                 ServiceNotification.notify(context, 0, null)
+                Assert.assertEquals(intent.action, Scheduler.ONE_SHOOT_ACTION)
             }
         }
     }
