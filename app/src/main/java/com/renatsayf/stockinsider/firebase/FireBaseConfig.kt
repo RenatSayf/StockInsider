83f187d730f854fac9cda4328c9668816093eadd
Renat <renatsayf@gmail.com>
2023-01-26 17:37:06 +0500

    FireBaseConfig.kt, DonateDialog.kt - bug fixing

--- app/src/main/java/com/renatsayf/stockinsider/firebase/FireBaseConfig.kt
+++ app/src/main/java/com/renatsayf/stockinsider/firebase/FireBaseConfig.kt
@@ -14,12 +14,10 @@ import com.renatsayf.stockinsider.models.Source
 
 object FireBaseConfig {
 
-    var userAgent = Firebase.remoteConfig.getString("user_agent")
-    var workerPeriod = if(BuildConfig.DEBUG && BuildConfig.DATA_SOURCE == Source.LOCALHOST.name) 60L
-    else Firebase.remoteConfig.getLong("worker_period")
-    var requestsCount = if(BuildConfig.DEBUG) 3 else Firebase.remoteConfig.getLong("requests_count").toInt()
-    private var sanctionsStr = Firebase.remoteConfig.getValue("sanctions_list").asString()
-    val sanctionsArray: Array<String> = Gson().fromJson(sanctionsStr, Array<String>::class.java)
+    var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    var workerPeriod = 240L
+    var requestsCount = 5
+    var sanctionsArray: Array<String> = arrayOf("RU","BY","IR","CU","KP","SY")
 
     private val configSettings = remoteConfigSettings {
         minimumFetchIntervalInSeconds = 3600
@@ -29,11 +27,26 @@ object FireBaseConfig {
         Firebase.remoteConfig.apply {
             setConfigSettingsAsync(configSettings)
             setDefaultsAsync(R.xml.remote_config_defaults)
-            fetchAndActivate().addOnFailureListener(object : OnFailureListener {
-                override fun onFailure(e: Exception) {
-                    if (BuildConfig.DEBUG) e.printStackTrace()
+            fetchAndActivate()
+                .addOnFailureListener(object : OnFailureListener {
+                    override fun onFailure(e: Exception) {
+                        if (BuildConfig.DEBUG) e.printStackTrace()
+                    }
+                }).addOnSuccessListener {
+
+                    userAgent = Firebase.remoteConfig.getString("user_agent")
+
+                    workerPeriod = if(BuildConfig.DEBUG && BuildConfig.DATA_SOURCE == Source.LOCALHOST.name) 60L
+                    else Firebase.remoteConfig.getLong("worker_period")
+
+                    requestsCount = if(BuildConfig.DEBUG) 3 else Firebase.remoteConfig.getLong("requests_count").toInt()
+
+                    val sanctionsStr = Firebase.remoteConfig.getString("sanctions_list")
+                    sanctionsArray = if (sanctionsStr.isNotEmpty()) {
+                        Gson().fromJson(sanctionsStr, Array<String>::class.java)
+                    }
+                    else arrayOf("RU","BY","IR","CU","KP","SY")
                 }
-            })
         }
     }
 }
