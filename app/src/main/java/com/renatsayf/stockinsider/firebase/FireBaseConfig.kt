2782ac34b21139b68ab69ecf307cedd86803ff91
Renat <renatsayf@gmail.com>
2023-01-26 20:19:33 +0500

    FireBaseConfig.kt - refactoring

--- app/src/main/java/com/renatsayf/stockinsider/firebase/FireBaseConfig.kt
+++ app/src/main/java/com/renatsayf/stockinsider/firebase/FireBaseConfig.kt
@@ -14,10 +14,31 @@ import com.renatsayf.stockinsider.models.Source
 
 object FireBaseConfig {
 
-    var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-    var workerPeriod = 240L
-    var requestsCount = 5
-    var sanctionsArray: Array<String> = arrayOf("RU","BY","IR","CU","KP","SY")
+    val userAgent: String
+        get() {
+            val value = Firebase.remoteConfig.getString("user_agent")
+            return value.ifEmpty { "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" }
+        }
+    val workerPeriod: Long
+        get() {
+            val value = Firebase.remoteConfig.getLong("worker_period")
+            return if (value == 0L || (BuildConfig.DEBUG && BuildConfig.DATA_SOURCE == Source.LOCALHOST.name)) 60L
+            else value
+        }
+    val requestsCount: Int
+        get() {
+            val value = Firebase.remoteConfig.getLong("requests_count").toInt()
+            return if (value == 0 || BuildConfig.DEBUG) 3
+            else value
+        }
+    val sanctionsArray: Array<String>
+        get() {
+            val value = Firebase.remoteConfig.getString("sanctions_list")
+            return if (value.isNotEmpty()) {
+                Gson().fromJson(value, Array<String>::class.java)
+            }
+            else arrayOf("RU","BY","IR","CU","KP","SY")
+        }
 
     private val configSettings = remoteConfigSettings {
         minimumFetchIntervalInSeconds = 3600
@@ -32,21 +53,7 @@ object FireBaseConfig {
                     override fun onFailure(e: Exception) {
                         if (BuildConfig.DEBUG) e.printStackTrace()
                     }
-                }).addOnSuccessListener {
-
-                    userAgent = Firebase.remoteConfig.getString("user_agent")
-
-                    workerPeriod = if(BuildConfig.DEBUG && BuildConfig.DATA_SOURCE == Source.LOCALHOST.name) 60L
-                    else Firebase.remoteConfig.getLong("worker_period")
-
-                    requestsCount = if(BuildConfig.DEBUG) 3 else Firebase.remoteConfig.getLong("requests_count").toInt()
-
-                    val sanctionsStr = Firebase.remoteConfig.getString("sanctions_list")
-                    sanctionsArray = if (sanctionsStr.isNotEmpty()) {
-                        Gson().fromJson(sanctionsStr, Array<String>::class.java)
-                    }
-                    else arrayOf("RU","BY","IR","CU","KP","SY")
-                }
+                })
         }
     }
 }
