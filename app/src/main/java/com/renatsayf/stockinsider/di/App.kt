36a8ce1f3d57050e0346f718de6234df89eb9c4f
Renat <renatsayf@gmail.com>
2023-05-29 22:56:08 +0500

    AppMetrica has been enabled

--- app/src/main/java/com/renatsayf/stockinsider/di/App.kt
+++ app/src/main/java/com/renatsayf/stockinsider/di/App.kt
@@ -5,11 +5,23 @@ package com.renatsayf.stockinsider.di
 import android.app.Application
 import android.util.Log
 import androidx.work.Configuration
+import com.renatsayf.stockinsider.R
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class App : Application(), Configuration.Provider {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val apiKey = getString(R.string.ya_metrica_api_key)
+        val config = YandexMetricaConfig.newConfigBuilder(apiKey).build()
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
+    }
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder().setMinimumLoggingLevel(Log.DEBUG).build()
