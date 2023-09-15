7532bd73dbbc0657195196b9c587d7c26bef161b
Renat <renatsayf@gmail.com>
2023-09-15 21:12:27 +0500

    MessagingService.kt has been added

---
+++ app/src/main/java/com/renatsayf/stockinsider/firebase/push/MessagingService.kt
@@ -0,0 +1,15 @@
+package com.renatsayf.stockinsider.firebase.push
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+
+    }
+}
